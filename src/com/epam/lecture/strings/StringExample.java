d706454c4112c8d7e72a2ace8a20ed054e3b9d8f
account0809 <namelina01@gmail.com>
2023-03-13 12:25:42 -0400

    [0000] Implement string pool example

---
+++ src/com/epam/lecture/strings/StringExample.java
@@ -0,0 +1,12 @@
+package com.epam.lecture.strings;
+
+public class StringExample {
+
+    public static void main(String[] args) {
+        String text1 = "Strings and Arrays";
+        String text2 = "Strings and Arrays";
+
+        System.out.println(text1 == text2);
+        System.out.println(text1.equals(text2));
+    }
+}
